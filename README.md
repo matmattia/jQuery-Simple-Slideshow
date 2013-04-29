d3338149b54f0f3c66061234d70e638bd23a3e23
Mattia <info@matriz.it>
2013-04-29 19:10:17 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# jQuery Custom Select
+
+jQuery Custom Select is a jQuery plugin to customize selects with CSS.
+
+## More info
+
+[jQuery Custom Select - Matriz.it](http://www.matriz.it/projects/jquery-custom-select/ "Matriz | Projects | jQuery-Custom-Select")
+
+## License
+
+This plugin is licensed under the MIT license.
+
+## Credits
+
+Mattia at [Matriz.it](http://www.matriz.it/) (info@matriz.it)
---
+++ jquery.slideshow.js
@@ -0,0 +1,85 @@
+/**
+* jQuery Simple Slideshow Select
+* @name jquery.slideshow.js
+* @author Mattia - http://www.matriz.it
+* @version 1.0.0
+* @date April 23, 2013
+* @category jQuery plugin
+* @copyright (c) 2013 Mattia at Matriz.it (info@matriz.it)
+* @license MIT - http://opensource.org/licenses/mit-license.php
+* @example Visit http://www.matriz.it/projects/jquery-simple-slideshow/ for more informations about this jQuery plugin
+*/
+(function($) {
+	var slideshow = {
+		'init': function(el, opts) {
+			var t = $(el),
+				children = this.getChildren(el, opts),
+				active = null;
+			if (children.length > 1) {
+				active = this.getActive(el, opts);
+				if (t.css('position') == 'static') {
+					t.css('position', 'relative');
+				}
+				children.css({
+					'position': 'absolute',
+					'top': 0,
+					'left': 0,
+					'opacity': 0
+				});
+				if (active.length > 0) {
+					active.css('opacity', 1);
+				}
+			}
+		},
+		'next': function(el, opts) {
+			var t = $(el),
+				children = this.getChildren(el, opts),
+				active = null, next = null;
+			if (children.length > 1) {
+				active = this.getActive(el, opts);
+				next = active.next();
+				if (next.length == 0) {
+					next = t.find(opts.children_tag + ':first-child');
+				}
+				active.addClass(opts.last_active_class);
+				next.css('opacity', 0).addClass(opts.active_class).animate({
+					'opacity': 1
+				}, opts.duration, function() {
+					active.removeClass(opts.active_class).removeClass(opts.last_active_class).css('opacity', 0);
+				});
+			}
+		},
+		'getChildren': function(el, opts) {
+			return $(el).find(opts.children_tag);
+		},
+		'getActive': function(el, opts) {
+			var t = $(el),
+				active = t.find(opts.children_tag + '.' + opts.active_class);
+			if (active.length == 0) {
+				active = t.find(opts.children_tag + ':first-child');
+				if (active.length > 0) {
+					active.addClass(opts.active_class);
+				}
+			}
+			return active;
+		}
+	};
+
+	$.fn.slideshow = function(options) {
+		var opts = $.extend({
+			'duration': 1000,
+			'pause': 5000,
+			'children_tag': 'img',
+			'active_class': 'active',
+			'last_active_class': 'last-active'
+		}, options);
+		
+		return this.each(function() {
+			var t = this;
+			slideshow.init(t, opts);
+			setInterval(function() {
+				slideshow.next(t, opts);
+			}, opts.pause);
+		});
+	};
+})(jQuery);
